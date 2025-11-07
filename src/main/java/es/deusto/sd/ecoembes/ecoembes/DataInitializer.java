a43d1a7995a4f82b15611e363856c1d3e068464c
ikeiba <ibarrola.iker@opendeusto.es>
2025-11-07 11:31:19 +0100

    Carpeta con la aplicacion principañ

--- src/main/java/es/deusto/sd/ecoembes/ecoembes/DataInitializer.java
+++ src/main/java/es/deusto/sd/ecoembes/ecoembes/DataInitializer.java
@@ -3,7 +3,7 @@
  * adapted using GitHub Copilot. It has been thoroughly reviewed 
  * and validated to ensure correctness and that it is free of errors.
  */
-package es.deusto.sd.ecoembes;
+package es.deusto.sd.ecoembes.ecoembes;
 
 import java.time.LocalDate; // Importante para las fechas de historial
 import org.slf4j.Logger;
