14152fb2efbeb2cbb5f593bfd780ddcf5b6f517d
ikeiba <ibarrola.iker@opendeusto.es>
2025-11-27 12:31:27 +0100

    solucionar errores

--- EcoembesServer/src/main/java/es/deusto/sd/ecoembes/external/PlasSBServerGateway.java
+++ EcoembesServer/src/main/java/es/deusto/sd/ecoembes/external/PlasSBServerGateway.java
@@ -15,7 +15,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import es.deusto.sd.ecoembes.dto.AssignmentExternalNotificationDTO;
 
